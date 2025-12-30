53d073bcd5f573020d73a2a7f165c89c30293e6c
Cyril Li <cyrils.li@icloud.com>
2025-12-30 02:58:26 -0600

    implement media session seek and play/pause

--- src/static/media_session.js
+++ src/static/media_session.js
@@ -1,160 +1,70 @@
 function createSilentAudio(time) {
-    const bufferLength = time * 44100;
-    const AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
-    if (!AudioContext) {
-        console.log("No Audio Context")
+    const dataSize = Math.ceil(time * 8000);
+    const byteLength = 44 + dataSize;
+    const view = new DataView(new ArrayBuffer(byteLength));
+
+    view.setUint32(0, 0x52494646, false);    // Chunk ID "RIFF"
+    view.setUint32(4, 36 + dataSize, true);  // File size
+    view.setUint32(8, 0x57415645, false);    // Format "WAVE"
+    view.setUint32(12, 0x666D7420, false);   // Sub-chunk 1 ID "fmt "
+    view.setUint32(16, 16, true);            // Sub-chunk 1 size
+    view.setUint16(20, 1, true);             // Audio format
+    view.setUint16(22, 1, true);             // Number of channels
+    view.setUint32(24, 8000, true);          // Sample rate
+    view.setUint32(28, 8000, true);          // Byte rate
+    view.setUint16(32, 1, true);             // Block align
+    view.setUint16(34, 8, true);             // Bits per sample
+    view.setUint32(36, 0x64617461, false);   // Sub-chunk 2 ID "data"
+    view.setUint32(40, dataSize, true);      // Sub-chunk 2 size
+
+    for (let offset = 44; offset < byteLength; offset++) {
+        view.setUint8(offset, 128);
     }
-    const context = new AudioContext();
-    const audioFile = context.createBuffer(1, bufferLength, 44100);
-
-    let numOfChan = audioFile.numberOfChannels,
-        length = bufferLength * numOfChan * 2 + 44,
-        buffer = new ArrayBuffer(length),
-        view = new DataView(buffer),
-        channels = [], i, sample,
-        offset = 0,
-        pos = 0;
-
-    function setUint16(data) {
-        view.setUint16(pos, data, true);
-        pos += 2;
-    }
-
-    function setUint32(data) {
-        view.setUint32(pos, data, true);
-        pos += 4;
-    }
-
-    setUint32(0x46464952);
-    setUint32(length - 8);
-    setUint32(0x45564157);
-
-    setUint32(0x20746d66);
-    setUint32(16);
-    setUint16(1);
-    setUint16(numOfChan);
-    setUint32(audioFile.sampleRate);
-    setUint32(audioFile.sampleRate * 2 * numOfChan);
-    setUint16(numOfChan * 2);
-    setUint16(16);
-
-    setUint32(0x61746164);
-    setUint32(length - pos - 4);
-
-    for (i = 0; i < audioFile.numberOfChannels; i++)
-        channels.push(audioFile.getChannelData(i));
-
-    while (pos < length) {
-        for (i = 0; i < numOfChan; i++) {
-            sample = Math.max(-1, Math.min(1, channels[i][offset]));
-            sample = (0.5 + sample < 0 ? sample * 32768 : sample * 32767) | 0;
-            view.setInt16(pos, sample, true);
-            pos += 2;
-        }
-        offset++;
-    }
-    return URL.createObjectURL(new Blob([buffer], { type: "audio/wav" }));
+    return URL.createObjectURL(new Blob([view], { type: "audio/wav" }));
 }
 
 const audioEl = document.createElement("audio");
-window.audioEl = audioEl;
-// window.mediaSessionAction = null;
-audioEl.src = createSilentAudio(0.01);
 
-window.updateMedia = (length, position) => {
+window.updateMedia = (length, position, playing) => {
     if (length !== audioEl.duration) {
         audioEl.src = createSilentAudio(length);
     }
-    audioEl.play().then(() => navigator.mediaSession.setPositionState({
-        duration: audioEl.duration,
-        playbackRate: audioEl.playbackRate,
-        position: position
-    })).catch(() => {});
+    audioEl.play().then(() => {
+        if (!playing) {
+            audioEl.pause();
+        }
+        navigator.mediaSession.setPositionState({
+            duration: audioEl.duration,
+            playbackRate: 1,
+            position: position
+        });
+    }).catch(() => {});
 }
 
 window.stopMediaSession = () => {
-    updateMedia(0.01, 0);
+    updateMedia(0.1, 0, false);
 }
 
 window.clearMediaSession = () => {
     audioEl.src = "";
 }
 
-/* window.updateMedia = function(length, position, isPlaying) {
-    if (length !== audioEl.duration) {
-        audioEl.src = createSilentAudio(length);
-    }
-    audioEl.load();
-    audioEl.play().then(() => window.updateMetadata()).catch(() => {});
-    console.log("isPlaying", isPlaying);
-    if (!isPlaying) {
-        audioEl.pause();
-    }
-    audioEl.currentTime = position;
-}
-
-window.updateMetadata = function(title, artist, album, artwork) {
-    navigator.mediaSession.metadata = new MediaMetadata({
-        title: title || "Title N/A",
-        artist: artist || "Artist N/A",
-        album: album || "Album N/A",
-        artwork: artwork || []
-    });
-    updatePositionState();
-}
-
-function updatePositionState() {
-    if ("setPositionState" in navigator.mediaSession) {
-        navigator.mediaSession.setPositionState({
-            duration: audioEl.duration,
-            playbackRate: audioEl.playbackRate,
-            position: audioEl.currentTime
-        });
-    }
-}
-
-navigator.mediaSession.setActionHandler("previoustrack", () => {
-    window.dispatchEvent(new CustomEvent("mediaSessionEvent", { detail: { type: "previousTrack" } }));
+navigator.mediaSession.setActionHandler("seekbackward", () => {
+    window.mediaSessionEvent("seekbackward", null);
 });
 
-navigator.mediaSession.setActionHandler("nexttrack", () => {
-    window.dispatchEvent(new CustomEvent("mediaSessionEvent", { detail: { type: "nextTrack" } }));
+navigator.mediaSession.setActionHandler("seekforward", () => {
+    window.mediaSessionEvent("seekforward", null);
 });
 
-navigator.mediaSession.setActionHandler("seekforward", (ev) => {
-    window.mediaSessionAction = "seekForward";
-    console.log("seek forward");
-    audioEl.currentTime += 10;
-    updatePositionState();
-});
-
-navigator.mediaSession.setActionHandler("seekbackward", (ev) => {
-    window.mediaSessionAction = "seekBackward";
-    console.log("seek backward");
-    audioEl.currentTime -= 10;
-    updatePositionState();
-});
-
-navigator.mediaSession.setActionHandler("play", async () => {
-    window.dispatchEvent(new CustomEvent("mediaSessionEvent", { detail: { type: "play-pause" }}));
-    await audioEl.play();
+navigator.mediaSession.setActionHandler("play", () => {
+    window.mediaSessionEvent("play", null);
 });
 
 navigator.mediaSession.setActionHandler("pause", () => {
-    window.dispatchEvent(new CustomEvent("mediaSessionEvent", { detail: { type: "play-pause" }}));
-    audioEl.pause();
-});
-
-audioEl.addEventListener("play", () => {
-    navigator.mediaSession.playbackState = "playing";
-});
-
-audioEl.addEventListener("pause", () => {
-    navigator.mediaSession.playbackState = "paused";
+    window.mediaSessionEvent("pause", null);
 });
 
 navigator.mediaSession.setActionHandler("seekto", (ev) => {
-    window.dispatchEvent(new CustomEvent("mediaSessionEvent", { detail: {
-        type: "seekTo", seekTime: ev.seekTime 
-    }}));
-}); */
+    window.mediaSessionEvent("seekto", ev.seekTime);
+});
